f4f3a936a55fd61989f2441fe45b54522346fcff
Colby Austin Brown <colbyaustinbrown@gmail.com>
2024-07-10 20:01:51 -0700

    End of class 7/10

--- sudoku/Sudoku.java
+++ sudoku/Sudoku.java
@@ -15,14 +15,33 @@ class Sudoku {
             }
         }
 
-        // SOLVE THE PUZZLE HERE
-        
-        for (int y = 0; y < 9; y++) {
-            for (int x = 0; x < 9; x++) {
-                System.out.print(board[y][x]);
+        if (solve(board, 0, 0)) { 
+            for (int y = 0; y < 9; y++) {
+                for (int x = 0; x < 9; x++) {
+                    System.out.print(board[y][x]);
+                }
+                System.out.println();
             }
-            System.out.println();
+        } else {
+            System.out.println("No Solution");
         }
+    }
+
+    // Generating guesses "in-place", i.e., the board parameter will be modified
+    // by this method, return true if it found a valid solution
+    // Otherwise, returns false and *returns board to its original state*
+    static boolean solve(int[][] board, int y, int x) {
+        if (y == 9) return true;
+        if (x == 9) return solve(board, y + 1, 0);
+        if (board[y][x] != 0) return solve(board, y, x + 1);
+
+        for (int i = 1; i <= 9; i++) {
+            board[y][x] = i;
+            if (!isValid(board)) continue;
+            if (solve(board, y, x + 1)) return true;
+        }
+        board[y][x] = 0;
+        return false;
     }
 
     static boolean isValid(int[][] board) {
@@ -30,7 +49,7 @@ class Sudoku {
         for (int y = 0; y < 9; y++) {
             boolean[] seen = new boolean[9];
             for (int x = 0; x < 9; x++) {
-                if (board[y][x] == 0) return false;
+                if (board[y][x] == 0) continue;
                 if (seen[board[y][x] - 1]) return false;
                 seen[board[y][x] - 1] = true;
             }
@@ -40,7 +59,7 @@ class Sudoku {
         for (int x = 0; x < 9; x++) {
             boolean[] seen = new boolean[9];
             for (int y = 0; y < 9; y++) {
-                if (board[y][x] == 0) return false;
+                if (board[y][x] == 0) continue;
                 if (seen[board[y][x] - 1]) return false;
                 seen[board[y][x] - 1] = true;
             }
@@ -50,7 +69,7 @@ class Sudoku {
         boolean[][][] seen = new boolean[3][3][9];
         for (int y = 0; y < 9; y++) {
             for (int x = 0; x < 9; x++) {
-               if (board[y][x] == 0) return false;
+               if (board[y][x] == 0) continue;
                if (seen[y / 3][x / 3][board[y][x] - 1]) return false;
                seen[y / 3][x / 3][board[y][x] - 1] = true;
             }
